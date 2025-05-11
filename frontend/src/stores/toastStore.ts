392bc397e47ceb9e9db99de35ae603f68b446b44
JeongSeoyun <xeonj@naver.com>
2025-05-11 22:42:32 +0900

    feat: Toast 상태 관리 구현

---
+++ frontend/src/stores/toastStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type Toast = {
+  id: string;
+  text: string;
+  sec?: number;
+  type?: "error" | "default";
+}
+
+type ToastStore = {
+  toasts: Toast[];
+  addTost: (text: string, sec?: number, type?: "error" | "default") => void;
+}
+
+export const useToastStore = create<ToastStore>() (
+  devtools((set) => ({
+    toasts: [],
+
+    addTost: (text, sec = 3, type="error") => {
+      const id = Date.now().toString();
+      set((state) => ({
+        toasts: [...state.toasts, {id, text, sec, type}].slice(-3),
+      }));
+
+      setTimeout(() => {
+        set((state) => ({
+          toasts: state.toasts.filter((toast) => toast.id !== id),
+        }))
+      }, sec * 1000);
+    },
+  }))
+);
