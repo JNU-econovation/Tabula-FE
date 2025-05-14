6abe6db07548bd16538958d961e898a2ebbfe52f
GimGayeon <kky1373@naver.com>
2025-05-14 16:13:43 +0900

    fix: Button 컴포넌트 스토리 폴더 수정

--- frontend/src/components/common/Button/Button.stories.tsx
+++ frontend/src/components/common/Button/Button.stories.tsx
@@ -1,64 +1,63 @@
-import * as React from "react"
-import { Meta, StoryObj } from "@storybook/react";
-import { FaSearch } from "react-icons/fa";
-import { Button } from "./Button";
-
+import * as React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { FaSearch } from 'react-icons/fa';
+import { Button } from './Button';
 
 const meta: Meta<typeof Button> = {
-  title: "Button",
+  title: 'common/Button',
   component: Button,
-  tags: ["autodocs"],
+  tags: ['autodocs'],
   args: {
-    children: "버튼",
+    children: '버튼',
   },
   parameters: {
     layout: 'centered',
   },
-}
+};
 
-export default meta
-type Story = StoryObj<typeof Button>
+export default meta;
+type Story = StoryObj<typeof Button>;
 
 export const FilledGradientButton: Story = {
   args: {
-    variant: "filled",
-    colorScheme: "gradient",
-    children: "무료로 시작하기"
+    variant: 'filled',
+    colorScheme: 'gradient',
+    children: '무료로 시작하기',
   },
-}
+};
 
 export const LineGrayButton: Story = {
   args: {
-    variant: "line",
-    colorScheme: "secondary",
-    children: "게스트로 시작하기"
-  }
-}
+    variant: 'line',
+    colorScheme: 'secondary',
+    children: '게스트로 시작하기',
+  },
+};
 
 export const LineGradientButton: Story = {
   args: {
-    variant: "line",
-    colorScheme: "gradient",
+    variant: 'line',
+    colorScheme: 'gradient',
     icon: <FaSearch />,
-    radius: "full",
-    children: "Tabula 사용법 알아보기",
+    radius: 'full',
+    children: 'Tabula 사용법 알아보기',
   },
-}
+};
 
 export const UploadButton: Story = {
   args: {
-    variant: "filled",
-    colorScheme: "primary",
-    radius: "md",
-    children: "업로드",
+    variant: 'filled',
+    colorScheme: 'primary',
+    radius: 'md',
+    children: '업로드',
   },
-}
+};
 
 export const LoginButton: Story = {
   args: {
-    variant: "filled",
-    colorScheme: "secondary",
-    radius: "full",
-    children: "로그인",
+    variant: 'filled',
+    colorScheme: 'secondary',
+    radius: 'full',
+    children: '로그인',
   },
-}
+};
