7aed947795ab5d0ed690800fb00d19d7a67ca836
GimGayeon <kky1373@naver.com>
2025-05-15 22:38:18 +0900

    feat: workspaceList get 핸들러 구현

---
+++ frontend/src/mocks/handlers.ts
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from 'msw';
+
+import workspaceList from './workspaceList.json';
+import { BASE_URL, END_POINT } from '@/api';
+
+export const handlers = [
+  http.get(`${BASE_URL}${END_POINT.workspaceList}:id`, async () => {
+    return HttpResponse.json(workspaceList, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }),
+];
