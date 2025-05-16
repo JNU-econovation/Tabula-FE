9ab29b3d646fe89d1fbdfd41c5a5ea5fa9aeed20
JeongSeoyun <xeonj@naver.com>
2025-05-16 22:17:11 +0900

    feat: 폴더 페이지 UI 구현

---
+++ frontend/app/subject/page.tsx
@@ -0,0 +1,11 @@
+import SubjectList from "@/components/Subject/SubjectList"
+
+const page = () => {
+  return (
+    <div className="px-60 py-30">
+      <SubjectList />
+    </div>
+  )
+}
+
+export default page
