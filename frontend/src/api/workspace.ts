0009579f0fa26f7040ca6f6d07cf8da19f03960a
GimGayeon <kky1373@naver.com>
2025-05-20 14:32:10 +0900

    feat: 워크스페이스 api 추가

--- frontend/src/api/workspace.ts
+++ frontend/src/api/workspace.ts
@@ -1,6 +1,41 @@
 import { AxiosInstance } from '@/api';
 
-export const getWorkspaceList = async (spaceId: string) => {
-  const response = await AxiosInstance.get(`/v1/spaces/${spaceId}`);
-  return response.data;
+interface WorkspaceList {
+  spaceId: string;
+  spaceName: string;
+}
+interface WorkspaceListResponse {
+  response: WorkspaceList[];
+}
+
+export const getWorkspaceList = async (
+  folderId: string,
+): Promise<WorkspaceListResponse> => {
+  const data = await AxiosInstance.get(`/v1/spaces/${folderId}`);
+
+  return data.data;
+};
+
+// TODO: 추후 에러 코드 별 처리 필요
+export const createWorkspace = async (folderId: string) => {
+  const data = await AxiosInstance.post(`/v1/spaces/${folderId}`);
+
+  return data.data;
+};
+
+export const deleteWorkspace = async (spaceId: string) => {
+  const data = await AxiosInstance.delete(`/v1/spaces/${spaceId}`);
+
+  return data.data;
+};
+
+export const updateWorkspaceName = async (
+  spaceId: string,
+  newSpaceName: string,
+) => {
+  const data = await AxiosInstance.put(`/v1/spaces/${spaceId}`, {
+    newSpaceName,
+  });
+
+  return data.data;
 };
