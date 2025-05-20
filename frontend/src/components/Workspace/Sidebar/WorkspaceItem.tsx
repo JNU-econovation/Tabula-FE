a1dad0bddfc95f84d6e277251e6bc6021cff06d5
GimGayeon <kky1373@naver.com>
2025-05-20 14:33:52 +0900

    feat: 워크스페이스 아이템 구현 - 워크스페이스 이름 변경, 삭제 - 수정 input 및 삭제 모달 - 삭제, 변경 메뉴 스페이스 구현

--- frontend/src/components/Workspace/Sidebar/WorkspaceItem.tsx
+++ frontend/src/components/Workspace/Sidebar/WorkspaceItem.tsx
@@ -1,6 +1,17 @@
 'use client';
 
+import { Button } from '@/components/common/Button/Button';
+import FloatingMenu from '@/components/common/FloatingMenu/FloatingMenu';
+import MenuItem from '@/components/common/FloatingMenu/MenuItem';
+
+import Modal from '@/components/common/Modal/Modal';
+
 import { useHandleOutside } from '@/hooks/common/useHandleOutside';
+import useModal from '@/hooks/common/useModal';
+import {
+  useUpdateWorkspaceName,
+  useDeleteWorkspace,
+} from '@/hooks/query/workspace/mutation';
 import Link from 'next/link';
 import { useState } from 'react';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
@@ -10,9 +21,14 @@ interface WorkspaceItemProps {
   spaceName: string;
 }
 
+//TODO: 리팩토링 필요
 const WorkspaceItem = ({ spaceId, spaceName }: WorkspaceItemProps) => {
   const [isSpaceOpen, setIsSpaceOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
+  const { mutate: updateWorkspace } = useUpdateWorkspaceName();
+  const { mutate: deleteWorkspace } = useDeleteWorkspace();
+  const [spaceNameValue, setSpaceNameValue] = useState(spaceName);
+  const { isModalOpen, openModal, closeModal } = useModal();
 
   const { menuRef, buttonRef } = useHandleOutside(isSpaceOpen, () => {
     setIsSpaceOpen(false);
@@ -21,14 +37,13 @@ const WorkspaceItem = ({ spaceId, spaceName }: WorkspaceItemProps) => {
   const toggleSpaceOpen = (event: React.MouseEvent) => {
     event.stopPropagation();
     event.preventDefault();
+    setSpaceNameValue(spaceName);
     setIsSpaceOpen(!isSpaceOpen);
   };
 
   const handleDelete = async () => {
-    if (confirm('정말 삭제하시겠습니까?')) {
-      // TODO: 실제 API 연동 필요
-      console.log(`Deleting workspace ${spaceId}`);
-    }
+    await deleteWorkspace(spaceId);
+    closeModal();
   };
 
   const handleEdit = () => {
@@ -50,56 +65,89 @@ const WorkspaceItem = ({ spaceId, spaceName }: WorkspaceItemProps) => {
         {isEditing ? (
           <input
             type="text"
-            className="border border-gray-300 rounded-md px-2 py-1 w-full"
+            className="border border-gray-300 rounded-md px-2 py-1 w-full text-sm"
             placeholder="폴더 이름을 입력하세요"
-            defaultValue={spaceName}
             onBlur={() => setIsEditing(false)}
-            onKeyDown={(e) => {
+            onKeyDown={async (e) => {
               if (e.key === 'Enter') {
-                // TODO: api 요청 추가
                 setIsEditing(false);
+                await updateWorkspace({
+                  spaceId,
+                  newSpaceName: spaceNameValue,
+                });
+                setSpaceNameValue('');
+                setIsSpaceOpen(false);
               }
               e.stopPropagation();
             }}
-            onClick={(e) => e.stopPropagation()}
+            value={spaceNameValue}
+            onChange={(e) => setSpaceNameValue(e.target.value)}
+            onClick={(e) => e.preventDefault()}
             autoFocus
           />
         ) : (
-          <div className="text-sm text-gray-700 truncate w-full">
-            {spaceName}
-          </div>
+          <>
+            <div className="text-sm text-gray-700 truncate w-4/5">
+              {spaceName}
+            </div>
+            <div className="absolute right-1 pr-2">
+              <button
+                className="flex items-center gap-2 cursor-pointer hover:bg-gray-300 rounded-2xl p-2"
+                onClick={toggleSpaceOpen}
+                ref={buttonRef}
+              >
+                <HiOutlineDotsHorizontal />
+              </button>
+            </div>
+          </>
         )}
       </Link>
 
-      <div className="flex items-center pr-2">
-        <button
-          className="flex items-center gap-2 cursor-pointer hover:bg-gray-300 rounded-2xl p-2"
-          onClick={toggleSpaceOpen}
-          ref={buttonRef}
-        >
-          <HiOutlineDotsHorizontal />
-        </button>
-      </div>
-
       {isSpaceOpen && (
-        <div
-          ref={menuRef}
-          className="absolute right-1 top-full mt-1 w-32 bg-white rounded-md shadow-lg z-10 border border-gray-200"
+        <FloatingMenu
+          menuRef={menuRef}
+          buttonRef={buttonRef}
+          width="8rem"
+          offset={{ x: -35, y: -10 }}
         >
-          <div
-            className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
-            onClick={handleEdit}
-          >
-            수정
-          </div>
-          <div
-            className="px-4 py-2 text-sm text-red-600 hover:bg-gray-100 cursor-pointer"
-            onClick={handleDelete}
-          >
-            삭제
+          <MenuItem onClick={handleEdit}>이름 수정하기</MenuItem>
+          <MenuItem onClick={openModal} danger>
+            삭제하기
+          </MenuItem>
+        </FloatingMenu>
+      )}
+
+      <Modal
+        isOpen={isModalOpen}
+        close={closeModal}
+        size="sm"
+        isXButton={false}
+      >
+        <div className="flex flex-col items-center justify-center h-full">
+          <h2 className="text-lg font-semibold mb-4">정말 삭제하시겠습니까?</h2>
+          <p className="text-sm text-gray-600 mb-4">
+            삭제된 폴더는 복구할 수 없어요
+          </p>
+          <div className="flex gap-4">
+            <Button
+              size="sm"
+              onClick={handleDelete}
+              colorScheme="secondary"
+              className="hover:bg-red-300"
+            >
+              삭제
+            </Button>
+            <Button
+              size="sm"
+              onClick={closeModal}
+              colorScheme="gray"
+              variant="line"
+            >
+              취소
+            </Button>
           </div>
         </div>
-      )}
+      </Modal>
     </div>
   );
 };
