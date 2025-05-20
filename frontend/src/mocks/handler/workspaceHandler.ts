700c9eacf53107684fc5c0ab494f0e9c11740e2d
GimGayeon <kky1373@naver.com>
2025-05-20 14:17:59 +0900

    feat: 워크스페이스 handler 분리

---
+++ frontend/src/mocks/handler/workspaceHandler.ts
@@ -0,0 +1,76 @@
+import { http, HttpResponse } from 'msw';
+import workspaceList from '../data/workspaceList.json';
+
+import { BASE_URL, END_POINT } from '@/api';
+
+export const workspaceHandler = [
+  http.get(`${BASE_URL}${END_POINT.workspaceList}:id`, async () => {
+    return HttpResponse.json(workspaceList, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }),
+
+  http.post(`${BASE_URL}${END_POINT.workspaceList}:id`, async () => {
+    const newWorkspace = {
+      spaceId: workspaceList.response.length + 101,
+      spaceName: `New Workspace ${workspaceList.response.length + 1}`,
+    };
+
+    workspaceList.response.push(newWorkspace);
+
+    return HttpResponse.json(workspaceList, {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }),
+
+  http.put(
+    `${BASE_URL}${END_POINT.workspaceList}:id`,
+    async ({ request, params }) => {
+      const { id } = params;
+      const data = (await request.json()) as {
+        newSpaceName: string;
+      };
+      const { newSpaceName } = data;
+
+      const updatedWorkspaceList = workspaceList.response.map((workspace) => {
+        if (id && workspace.spaceId == Number(id)) {
+          return { ...workspace, spaceName: newSpaceName };
+        }
+        return workspace;
+      });
+      workspaceList.response = updatedWorkspaceList;
+
+      return HttpResponse.json(updatedWorkspaceList, {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    },
+  ),
+
+  http.delete(
+    `${BASE_URL}${END_POINT.workspaceList}:id`,
+    async ({ params }) => {
+      const { id } = params;
+
+      const updatedWorkspaceList = workspaceList.response.filter(
+        (workspace) => workspace.spaceId != Number(id),
+      );
+      workspaceList.response = updatedWorkspaceList;
+
+      return HttpResponse.json(updatedWorkspaceList, {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    },
+  ),
+];
