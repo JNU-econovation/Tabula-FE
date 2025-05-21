eaebbff34166b31aa5ad072c8dc36eaa92b6be87
GimGayeon <kky1373@naver.com>
2025-05-21 19:43:16 +0900

    feat: useLearningFile 훅 구현 - 학습자료 제목 상태관리 - 학습자료 언어 상태관리 - 학습자료 제출

---
+++ frontend/src/hooks/workspace/useLearningFile.ts
@@ -0,0 +1,53 @@
+import { useUploadLearningFile } from '@/hooks/query/workspace/mutation';
+import { useState } from 'react';
+
+const useLearningFile = (folderId: string, file: File | null) => {
+  const { mutate } = useUploadLearningFile();
+  const [subjectName, setSubjectName] = useState('');
+  const [language, setLanguage] = useState<'english' | 'korean' | 'japanese'>(
+    'korean',
+  );
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLanguage(e.target.value as 'english' | 'korean' | 'japanese');
+  };
+  const handleSubjectNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubjectName(e.target.value);
+  };
+  const resetSubjectName = () => {
+    setSubjectName('');
+  };
+
+  const uploadLearningFile = async (ref: HTMLInputElement) => {
+    if (!file) {
+      alert('파일을 선택해주세요.');
+      return;
+    }
+    if (!subjectName) {
+      alert('과목명을 입력해주세요.');
+      ref.focus();
+      return;
+    }
+    if (file.size > 10 * 1024 * 1024) {
+      alert('파일 크기는 10MB를 초과할 수 없습니다.');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('langType', language);
+    formData.append('fileDomain', subjectName);
+
+    return mutate({ folderId, formData });
+  };
+
+  return {
+    subjectName,
+    language,
+    uploadLearningFile,
+    handleLanguageChange,
+    handleSubjectNameChange,
+    resetSubjectName,
+  };
+};
+
+export default useLearningFile;
