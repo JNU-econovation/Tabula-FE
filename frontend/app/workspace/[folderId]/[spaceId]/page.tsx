4030594d09f37e55d3c83613126321fd55194d1d
JeongSeoyun <xeonj@naver.com>
2025-05-21 21:18:29 +0900

    feat: 워크스페이스 UserPanel 컴포넌트 구현

--- frontend/app/workspace/[folderId]/[spaceId]/page.tsx
+++ frontend/app/workspace/[folderId]/[spaceId]/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import UserPanel from '@/components/Workspace/User/UserPanel';
 import { useParams } from 'next/navigation';
 
 const page = () => {
   const { spaceId } = useParams();
 
-  return <div className="flex h-full">디테일공간</div>;
+  return (
+    <div className="relative flex w-full">
+      <div className='absolute top-15 right-5 sm:right-10 md:right-15 xl:right-20 space-y-4'>
+        <UserPanel
+          title="학습 자료"
+          fileTitle='파일1.pdf'
+         />
+      </div>
+    </div>
+  ) 
+  ;
 };
 
 export default page;
