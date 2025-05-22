70273438aafcb52178e27691a24f2fc3d7f89e86
JeongSeoyun <xeonj@naver.com>
2025-05-22 10:40:26 +0900

    style: SelectLaunguage 컴포넌트에 showLabel 속성 여부에 따른 스타일링 구현

--- frontend/src/components/Workspace/LearningFileUpload/SelectLanguage.tsx
+++ frontend/src/components/Workspace/LearningFileUpload/SelectLanguage.tsx
@@ -1,15 +1,19 @@
 interface SelectLanguageProps {
   language: 'english' | 'korean' | 'japanese';
   handleLanguageChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  showLabel?: boolean;
 }
 
 const SelectLanguage = ({
   language,
   handleLanguageChange,
+  showLabel = true,
 }: SelectLanguageProps) => {
   return (
-    <span className="flex items-center w-full gap-7">
-      <p>자료 언어 선택</p>
+    <span className={`flex items-center ${
+    showLabel ? 'w-full gap-7' : 'mr-5 gap-0'
+    }`}>
+      {showLabel && <p>자료 언어 선택</p>}
       <select
         className="bg-gray-100 rounded-xl px-2 py-2 text-sm text-gray-700 
              focus:border-primary-400 focus:bg-primary-50 
