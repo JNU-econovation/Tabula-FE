33f79a4dd4741476c01b01788e3fb5bc5ed9c9f1
JeongSeoyun <xeonj@naver.com>
2025-05-22 20:55:26 +0900

    feat: 업로드 컴포넌트 height xs 속성 추가 및 파일 타입 확장 구현

--- frontend/src/components/common/Upload/Upload.tsx
+++ frontend/src/components/common/Upload/Upload.tsx
@@ -7,14 +7,16 @@ import { SizeType } from '@/type';
 import useDragFile from '@/hooks/common/useDragFile';
 
 interface UploadProps {
-  height?: SizeType;
+  height?: SizeType | 'xs';
   width?: SizeType | 'full';
   processFile: (file: File) => void;
   handleFile: (e: React.ChangeEvent<HTMLInputElement>) => void;
   mode?: 'pdf-type'| 'multi-type';
+  selectedFile?: File | null;
+  imageFiles?: File[]
 }
 
-const Upload = ({ height, width, processFile, handleFile, mode = 'pdf-type' }: UploadProps) => {
+const Upload = ({ height, width, processFile, handleFile, mode = 'pdf-type', selectedFile, imageFiles = [] }: UploadProps) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
   const {
     isDragging,
@@ -35,6 +37,7 @@ const Upload = ({ height, width, processFile, handleFile, mode = 'pdf-type' }: U
     {
       variants: {
         height: {
+          xs: ['h-28', 'gap-1', 'text-sm', '[&>svg]:hidden'],
           sm: ['h-32', 'gap-1', 'text-sm', 'p-2', '[&>svg]:hidden'],
           md: ['h-64', 'gap-3', 'text-base', 'p-4'],
           lg: ['h-96', 'gap-6', 'text-lg', 'p-6'],
@@ -69,7 +72,7 @@ const Upload = ({ height, width, processFile, handleFile, mode = 'pdf-type' }: U
       onDrop={handleDrop}
     >
       <>
-        {height !== 'sm' && (
+        {height !== 'sm' && height !== 'xs'  && (
           <img className="w-7 h-7" src={uploadIcon.src} alt="Upload Icon" />
         )}
         <input
@@ -78,15 +81,21 @@ const Upload = ({ height, width, processFile, handleFile, mode = 'pdf-type' }: U
           className="hidden"
           accept={
             mode === 'pdf-type'
-              ? ".pdf"
-              : ".pdf,.png,.jpg,.jpeg"
+              ? '.pdf'
+              : selectedFile
+                ? '.pdf'
+                : imageFiles?.length > 0
+                  ? ".png,.jpg,.jpeg"
+                  : ".pdf,.png,.jpg,.jpeg"
           }
           multiple={mode === 'multi-type'}
           onChange={handleFile}
         />
         <div className="flex flex-col gap-1 items-center">
-          <p className="text-base">Choose a file or drag & drop it here</p>
-          <p className="text-sm text-gray-400">pdf 파일을 업로드 해 주세요</p>
+        {height !== 'xs' && (
+          <p className="text-base description">Choose a file or drag & drop it here</p>
+        )}
+          <p className="text-sm text-gray-400 mb-2 whitespace-">{height === 'xs' ? 'pdf 또는 png / jpg 파일을 업로드 해 주세요  (png / jpg는 여러 장 업로드 가능)' : 'pdf 파일을 업로드 해 주세요'}</p>
         </div>
         <button
           className="cursor-pointer px-5 py-2 bg-white text-black border border-gray-300 rounded-lg text-sm hover:bg-gray-100 transition-colors duration-200"
