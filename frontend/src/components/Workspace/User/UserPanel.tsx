5f1962e084ca57ed946c467e960a11a27d001749
GimGayeon <kky1373@naver.com>
2025-05-23 16:40:07 +0900

    fix: panel 및 upload 컴포넌트 크기 조절

--- frontend/src/components/Workspace/User/UserPanel.tsx
+++ frontend/src/components/Workspace/User/UserPanel.tsx
@@ -5,11 +5,13 @@ interface UserPanelProps {
 
 const UserPanel = ({ title, fileTitle }: UserPanelProps) => {
   return (
-    <div className="flex flex-col w-60 bg-gray-200 p-4 rounded-2xl border border-gray-300 gap-4">
+    <div className="flex flex-col w-50 h-40 bg-gray-200 p-4 rounded-2xl border border-gray-300 gap-4">
       <div className="font-semibold ml-2 text-gray-800">{title}</div>
-      <div className="rounded-xl bg-white ml-4 mr-1 px-6 py-8 text-gray-600 shadow-md">{fileTitle}</div>
+      <div className="rounded-xl bg-white ml-4 mr-1 px-6 py-8 text-gray-600 shadow-md">
+        {fileTitle}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserPanel
+export default UserPanel;
