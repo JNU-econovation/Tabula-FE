6fef1cbedbbbb4d71dc6187f0917bc6f2ae769e3
JeongSeoyun <xeonj@naver.com>
2025-05-26 22:31:01 +0900

    feat: 마이페이지 학습 통계 캘린더 UI 및 기능 구현

--- frontend/src/components/Mypage/StudyStats.tsx
+++ frontend/src/components/Mypage/StudyStats.tsx
@@ -13,6 +13,40 @@ const StudyStats = () => {
 
   const { response, isLoading } = useGetMypage(year, month)
 
+  const studyStatsMap: Record<string, number> = {}
+  response?.data.forEach(({ date, cnt }: StatsData) => {
+    const dateStr = date.split('T') ? date.split('T')[0] : date
+    studyStatsMap[dateStr] = cnt
+  })
+
+  const getLocalDateStr = (d: Date) => {
+    return `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${d
+      .getDate()
+      .toString()
+      .padStart(2, '0')}`
+  }
+
+  const tileClassName = ({ date }: { date: Date }) => {
+    const today = new Date()
+    const dateStr = getLocalDateStr(date)
+    const todayStr = getLocalDateStr(today)
+    const cnt = studyStatsMap[dateStr]
+
+    const classes = []
+
+    const level = Math.min(cnt, 3)
+    classes.push(`study-level-${level}`)
+
+    if (dateStr === todayStr) {
+      classes.push('today')
+    }
+
+    if (date > today) {
+      classes.push ('text-gray-300')
+    }
+
+    return classes.join(' ')
+  }
 
   const handleChange = (
     nextValue: Value,
@@ -36,14 +70,35 @@ const StudyStats = () => {
             onChange={handleChange}
             formatDay={(locale, date) => date.toLocaleDateString('en', { day: 'numeric' })}
             showNeighboringMonth={false}
-            minDetail="month"
+            minDetail="month" // 상단 네비게이션에서 '월' 단위만 보이게 설정
             maxDetail="month"
             tileDisabled={({ date }) => date > new Date()}
+            tileClassName={tileClassName}
             selectRange={false}
             prev2Label={null}
             next2Label={null}
             className='calendar-custom'
           />
+          <div className='flex items-center mt-4'>
+            {levels.map((level) =>
+              level === 3 ? (
+                <div key={level} className='flex items-center'>
+                  <div
+                    className={`flex w-8 h-4 justify-center items-center text-sm`}
+                    style={{ backgroundColor: COLOR_PALETTE.levelColors[level] }}
+                  >
+                    3+
+                  </div>
+                </div>
+              ) : (
+                <div key={level}
+                  className={`w-8 h-4 flex items-center justify-center text-sm`}
+                  style={{ backgroundColor: COLOR_PALETTE.levelColors[level] }}
+                >
+                  {level}
+                </div>
+              ))}
+          </div>
         </div>
       )}
     </div>
