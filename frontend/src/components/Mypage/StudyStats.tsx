75442e427e3a92880e6e0ca6dd1ae1e2a0fa4b80
JeongSeoyun <xeonj@naver.com>
2025-05-27 23:06:45 +0900

    refactor: 마이페이지 StudyStats 사용하지 않는 함수 제거

--- frontend/src/components/Mypage/StudyStats.tsx
+++ frontend/src/components/Mypage/StudyStats.tsx
@@ -3,10 +3,9 @@ import { COLOR_PALETTE } from '@/constants/color'
 import { useGetMypage } from '@/hooks/query/mypage/query'
 import { useState } from 'react'
 import Calendar from 'react-calendar'
-import { Value } from 'react-calendar/dist/esm/shared/types.js'
 
 const StudyStats = () => {
-  const [value, setValue] = useState(new Date())
+  const value = new Date()
   const year = value.getFullYear()
   const month = value.getMonth() + 1
   const levels = [0, 1, 2, 3]
@@ -61,17 +60,6 @@ const StudyStats = () => {
     return classes.join(' ')
   }
 
-  const handleChange = (
-    nextValue: Value,
-    _event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    if (nextValue instanceof Date) {
-      setValue(nextValue)
-    } else if (Array.isArray(nextValue) && nextValue[0] instanceof Date) {
-      setValue(nextValue[0])
-    }
-  }
-
   return (
     <div>
       {isLoading ? (
@@ -80,7 +68,6 @@ const StudyStats = () => {
         <div className='flex flex-col gap-1 mt-4 rounded-xl border border-gray-200 shadow-md p-4 w-fit'>
           <Calendar
             value={value}
-            onChange={handleChange}
             formatDay={(locale, date) => date.toLocaleDateString('en', { day: 'numeric' })}
             showNeighboringMonth={false}
             minDetail="month"
