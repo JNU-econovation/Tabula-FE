80d9aa91e4236756f47a5d3089c7ae8696c692ad
JeongSeoyun <xeonj@naver.com>
2025-05-28 14:55:56 +0900

    design: Modal 컴포넌트 X 버튼 클릭시 cursor pointer 적용

--- frontend/src/components/common/Modal/Modal.tsx
+++ frontend/src/components/common/Modal/Modal.tsx
@@ -58,7 +58,7 @@ const Modal = ({
       >
         {isXButton && (
           <button
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 hover:cursor-pointer"
             onClick={close}
           >
             <IoMdClose size={24} />
