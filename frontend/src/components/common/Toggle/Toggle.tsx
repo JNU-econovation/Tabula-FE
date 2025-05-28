2dae6ed88208be10150b46a2f6d5ad55c9f064c8
GimGayeon <kky1373@naver.com>
2025-05-28 14:29:46 +0900

    feat: 토글 공통 컴포넌트 구현

---
+++ frontend/src/components/common/Toggle/Toggle.tsx
@@ -0,0 +1,21 @@
+interface LayoutProps {
+  isToggleOn?: boolean;
+  handleToggle?: () => void;
+}
+
+const Toggle = ({ isToggleOn, handleToggle }: LayoutProps) => {
+  return (
+    <div
+      className={`${isToggleOn ? 'bg-gray-500' : 'bg-gray-300'} w-10 h-6 rounded-4xl flex items-center`}
+    >
+      <button
+        onClick={handleToggle}
+        className={`${
+          isToggleOn ? 'translate-x-5' : 'translate-x-1'
+        } transition-transform duration-300 ease-in-out bg-white w-4 h-4 rounded-full shadow-md cursor-pointer`}
+      ></button>
+    </div>
+  );
+};
+
+export default Toggle;
