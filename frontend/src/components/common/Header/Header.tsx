f1d7f9a312fc3e375c6954537530f89de1c6e33a
GimGayeon <kky1373@naver.com>
2025-05-28 13:09:51 +0900

    fix: Header fixed 되도록 수정

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,36 +1,40 @@
-"use client"
+'use client';
 
-import Image from 'next/image'
-import Link from 'next/link'
-import Logo from '../../../../assets/logo.png'
-import { AuthStore } from '@/stores/authStore'
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../../../assets/logo.png';
+import { AuthStore } from '@/stores/authStore';
 
 const Header = () => {
   const { isLogin, username } = AuthStore();
   return (
-    <div className='w-full h-18 flex items-center bg-white justify-between px-8 border-b border-gray-200'>
-      <Link href='/' className='flex items-center hover:scale-105 transition-transform duration-300'>
-        <Image src={Logo} alt='logo' width={40} height={40} />
+    <div className="w-full h-18 flex items-center bg-white justify-between px-8 border-b border-gray-200  fixed z-50">
+      <Link
+        href="/"
+        className="flex items-center hover:scale-105 transition-transform duration-300"
+      >
+        <Image src={Logo} alt="logo" width={40} height={40} />
       </Link>
 
-      <div className='flex items-center gap-9'>
+      <div className="flex items-center gap-9">
         <div>
-          <button className='flex items-center gap-2 px-4 py-2 text-sm text-indigo-600 border rounded-full hover:cursor-pointer hover:bg-[#F4F6FF] hover:shadow-[0_0_0_0.5px_#4F46E5] hover:outline-indigo-600 hover:outline-offset-0 transition-all duration-200'>
+          <button className="flex items-center gap-2 px-4 py-2 text-sm text-indigo-600 border rounded-full hover:cursor-pointer hover:bg-[#F4F6FF] hover:shadow-[0_0_0_0.5px_#4F46E5] hover:outline-indigo-600 hover:outline-offset-0 transition-all duration-200">
             🔍 Tabula 사용법 알아보기
           </button>
         </div>
-        
+
         {isLogin && username ? (
-          <div className='text-[#292929] mr-5'>{username}님</div>
+          <div className="text-[#292929] mr-5">{username}님</div>
         ) : (
-            <Link href='login'>
-              <button className='cursor-pointer bg-[#E7EAFF] px-5 py-2 rounded-full text-sm text-[#292929] hover:bg-[#d6d9f7] transition-all duration-300 hover:scale-103'>로그인</button>
-            </Link>
-          )
-        }
+          <Link href="login">
+            <button className="cursor-pointer bg-[#E7EAFF] px-5 py-2 rounded-full text-sm text-[#292929] hover:bg-[#d6d9f7] transition-all duration-300 hover:scale-103">
+              로그인
+            </button>
+          </Link>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
