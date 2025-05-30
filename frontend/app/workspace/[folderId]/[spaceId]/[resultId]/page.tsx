2f842b4fc7ed130d2acf7940afd1c619cefa3e58
GimGayeon <kky1373@naver.com>
2025-05-30 16:37:23 +0900

    fix: 자세히 보기 클릭 시 디테일 페이지로 이동하도록 수정

---
+++ frontend/app/workspace/[folderId]/[spaceId]/[resultId]/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import ResultDetail from '@/components/Workspace/ResultDetail';
+
+const page = () => {
+  return (
+    <div>
+      <ResultDetail />
+    </div>
+  );
+};
+
+export default page;
--- frontend/src/components/Workspace/StudyLog/Feedback.tsx
+++ frontend/src/components/Workspace/StudyLog/Feedback.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/common/Button/Button';
 import LoadingSpinner from '@/components/Workspace/StudyLog/LoadingSpinner';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
 import { FaSearchPlus } from 'react-icons/fa';
 
 interface FeedbackProps {
@@ -8,6 +10,7 @@ interface FeedbackProps {
   children: React.ReactNode;
 }
 const Feedback = ({ status, id, children }: FeedbackProps) => {
+  const { spaceId, folderId } = useParams();
   return (
     <div>
       {status === 'LOADING' ? (
@@ -19,21 +22,23 @@ const Feedback = ({ status, id, children }: FeedbackProps) => {
           <p>채점이 완료되었어요.</p>
           <p>자세히보기를 통해 채점결과에 대한 피드백을 확인할 수 있어요.</p>
           <div className="flex gap-5">{children}</div>
-          <Button
-            variant="line"
-            size="md"
-            style={{
-              border: '2px solid #2F0DA9',
-              color: '#2F0DA9',
-              width: '10rem',
-              borderRadius: '1rem',
-            }}
-          >
-            <div className="flex items-center gap-2">
-              <FaSearchPlus />
-              <p>자세히보기</p>
-            </div>
-          </Button>
+          <Link href={`/workspace/${folderId}/${spaceId}/${id}`}>
+            <Button
+              variant="line"
+              size="md"
+              style={{
+                border: '2px solid #2F0DA9',
+                color: '#2F0DA9',
+                width: '10rem',
+                borderRadius: '1rem',
+              }}
+            >
+              <div className="flex items-center gap-2">
+                <FaSearchPlus />
+                <p>자세히보기</p>
+              </div>
+            </Button>
+          </Link>
         </div>
       )}
     </div>
