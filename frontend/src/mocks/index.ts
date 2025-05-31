4bdffcdd3cc164922e99830977e19891be04b795
JeongSeoyun <xeonj@naver.com>
2025-05-31 21:33:51 +0900

    feat: msw 모킹 서버 비활성화

--- frontend/src/mocks/index.ts
+++ frontend/src/mocks/index.ts
@@ -1,4 +1,9 @@
 export async function initMocks() {
+
+  if (process.env.NEXT_PUBLIC_API_MOCKING !== 'enabled') {
+    return
+  }
+  
   if (typeof window === 'undefined') {
     const { server } = await import('./server');
     await server.listen();
