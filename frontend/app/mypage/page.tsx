d6082d05006cd8437fe6c3d95e7c461004040281
JeongSeoyun <xeonj@naver.com>
2025-06-05 16:40:06 +0900

    feat: 로그아웃 기능 구현

--- frontend/app/mypage/page.tsx
+++ frontend/app/mypage/page.tsx
@@ -2,24 +2,30 @@
 
 import FeedbackModal from "@/components/Mypage/FeedbackModal"
 import FeedbackSection from "@/components/Mypage/FeedbackSection"
+import LogoutSection from "@/components/Mypage/LogoutSection"
 import StatsSection from "@/components/Mypage/StatsSection"
 import UserInfo from "@/components/Mypage/UserInfo"
 import useModal from "@/hooks/common/useModal"
 import { useGuestRedirect } from "@/hooks/Login/useGuestRedirect"
+import { useLogout } from "@/hooks/mypage/useLogout"
 import { AuthStore } from "@/stores/authStore"
 
 const page = () => {
   const { username } = AuthStore()
   const { isModalOpen, openModal, closeModal } = useModal()
+  const { logout } = useLogout()
   const shouldRender = useGuestRedirect()
   if (!shouldRender) return null
 
   return (
     <div className="flex flex-col items-center p-15 min-h-calc(100vh-4.5rem)">
-      <div className="flex flex-col items-start gap-8 min-w-110">
+      <div className="flex flex-col items-start gap-6 min-w-110">
         <UserInfo username={username} />
         <StatsSection />
-        <FeedbackSection onClick={openModal} />
+        <div>
+          <FeedbackSection onClick={openModal} />
+          <LogoutSection onClick={logout} />
+        </div>
       </div>
       {isModalOpen && <FeedbackModal username={username} onClose={closeModal} />}
     </div>
