19dc88a5fdbd54da40f52c109f45750c48ede65d
GimGayeon <kky1373@naver.com>
2025-06-11 16:54:03 +0900

    fix: 로딩 컴포넌트 text, percent 없이 사용가능하도록 수정

--- frontend/src/components/common/Loading/Loading.tsx
+++ frontend/src/components/common/Loading/Loading.tsx
@@ -3,14 +3,19 @@ import Spinner from '@/components/common/Loading/Spinner';
 
 interface LoadingProps {
   type?: 'progress' | 'spinner';
-  text: string;
-  percent: number;
+  text?: string;
+  percent?: number;
 }
 const Loading = ({ type = 'progress', percent, text }: LoadingProps) => {
   return (
     <div className="w-full flex items-center justify-center p-8 relative">
-      {type === 'progress' && <ProgressBar text={text} percent={percent} />}
-      {type === 'spinner' && <Spinner text={text} percent={percent} />}
+      {type === 'progress' && text && percent && (
+        <ProgressBar text={text} percent={percent} />
+      )}
+      {type === 'spinner' && text && percent && (
+        <Spinner text={text} percent={percent} />
+      )}
+      {type === 'spinner' && !text && <Spinner />}
     </div>
   );
 };
