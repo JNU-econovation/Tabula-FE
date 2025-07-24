99c5f40ece0be770054d45ba00c24c7744e51e97
GimGayeon <kky1373@naver.com>
2025-07-24 20:31:40 +0900

    fix: 로그인 로직 back

---
+++ frontend/app/auth/callback/CallbackPage.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { postAuth } from '@/api/login';
+import { AuthStore } from '@/stores/authStore';
+import { useToastStore } from '@/stores/toastStore';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const CallbackPage = () => {
+  const addToast = useToastStore((state) => state.addToast);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const code = searchParams.get('code');
+  const setAuth = AuthStore((state) => state.setAuth);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      if (!code) {
+        alert('코드가 존재하지 않습니다.');
+        router.replace('/auth/error');
+        return;
+      }
+
+      try {
+        const data = await postAuth(code);
+
+        if (data.success) {
+          const { accessToken, refreshToken, userName } = data.response;
+
+          setAuth({
+            username: userName,
+            accessToken,
+            refreshToken,
+            loginType: 'user',
+          });
+          router.replace('/');
+        } else {
+          addToast('로그인에 실패했습니다. 다시 시도해주세요.');
+          router.replace('/auth/error');
+        }
+      } catch (error) {
+        console.log('에러 발생: ', error);
+        addToast('로그인 중 에러가 발생했습니다. 다시 시도해주세요.');
+        router.replace('/auth/error');
+      }
+    };
+    fetchToken();
+  }, [code, router, setAuth]);
+
+  return null;
+};
+
+export default CallbackPage;
--- frontend/src/hooks/Login/useGoogleLogin.ts
+++ frontend/src/hooks/Login/useGoogleLogin.ts
@@ -1,8 +1,21 @@
 export const useGoogleLogin = () => {
-
   const handleLogin = () => {
-    window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
-  }
+    const width = 500;
+    const height = 600;
 
-  return { handleLogin }
-}
+    const left = window.innerWidth / 2 + window.screenX - width / 2;
+    const top = window.innerHeight / 2 + window.screenY - height / 2;
+
+    const popup = window.open(
+      `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google`,
+      '_blank',
+      `width=${width},height=${height},left=${left},top=${top}`,
+    );
+
+    if (!popup) {
+      alert('팝업이 차단되었습니다. 팝업을 허용해주세요.');
+    }
+  };
+
+  return { handleLogin };
+};
