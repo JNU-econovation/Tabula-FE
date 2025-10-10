157e907c538446c4db18bada3330a31463128aa4
GimGayeon <kky1373@naver.com>
2025-10-10 17:03:32 +0900

    feat: 로딩 스피너에 애니메이션 추가

--- frontend/src/components/common/Loading/Spinner.tsx
+++ frontend/src/components/common/Loading/Spinner.tsx
@@ -1,18 +1,37 @@
 import { JSX } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
 interface SpinnerProps {
   text?: string | JSX.Element;
   percent?: number;
 }
 export default function Spinner({ text, percent }: SpinnerProps) {
   return (
-    <div className="flex items-center gap-5">
+    <div className="flex items-start gap-5">
       <div className="spinner-container">
         {Array.from({ length: 12 }, (_, i) => (
           <div key={i} className="spinner-line" />
         ))}
       </div>
-      {text && <p className="text-md text-gray-400">{text}</p>}
-      {percent && <p className="text-md text-gray-400">{percent}%</p>}
+      {percent !== undefined && (
+        <div className="text-md text-gray-400">{percent}%</div>
+      )}
+      {text && (
+        <div className="overflow-hidden flex-1 h-8 flex items-start">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={text as string}
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              className="w-full text-gray-400 text-md"
+            >
+              {text}
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      )}
     </div>
   );
 }
